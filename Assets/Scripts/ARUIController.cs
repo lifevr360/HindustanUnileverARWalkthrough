4f323d10a309a2300c4b710cd3678919f83c1887
Johnson <83759562+johnsonkj@users.noreply.github.com>
2026-06-21 18:12:10 +0530

    Added admin lights

--- Assets/Scripts/ARUIController.cs
+++ Assets/Scripts/ARUIController.cs
@@ -1,7 +1,19 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class ARUIController : MonoBehaviour
 {
+    public GameObject adminLight;
+    public GameObject sceneLight;
+
+    public Color ambientColor = Color.gray;
+
+    void Start()
+    {
+       adminLight.SetActive(false);
+       sceneLight.SetActive(true);  
+    }
+
     public void ShowPart(int index)
     {
         if (ARModelPartController.Instance != null)
@@ -20,5 +32,40 @@ public class ARUIController : MonoBehaviour
         {
             ARModelPartController.Instance.ShowAll();
         }
+    }
+
+    public void EnableAdminLights()
+    {
+       adminLight.SetActive(true);
+       sceneLight.SetActive(false);
+       SetAmbientColorMode();
+    }
+
+    public void DisableAdminLights()
+    {
+       adminLight.SetActive(false);
+       sceneLight.SetActive(true);
+       SetSkyboxMode();
+    }
+
+    public void SetAmbientColorMode()
+    {
+        // Change Source from Skybox to Color
+        RenderSettings.ambientMode = AmbientMode.Flat;
+
+        // Set the ambient color
+        RenderSettings.ambientLight = ambientColor;
+
+        // Refresh environment lighting
+        DynamicGI.UpdateEnvironment();
+    }
+
+    public void SetSkyboxMode()
+    {
+        // Change Source back to Skybox
+        RenderSettings.ambientMode = AmbientMode.Skybox;
+
+        // Refresh environment lighting
+        DynamicGI.UpdateEnvironment();
     }
 }
